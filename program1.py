efa0217cafe3c8009796f2a802d11c3db046697d
vinayvin576 <130123822+vinayvin576@users.noreply.github.com>
2024-11-11 13:28:59 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,5 +2,24 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-                    
-        return 0
+         if not grid:
+             return 0
+         rows,cols = len(grid),len(grid[0])
+         visited = [[false for _ in range(cols)] for _ in range(rows)]
+         def dfs(r,c):
+             if r < 0 or c < 0 or r >=rows or c>= cols or grid[r][c] == "W" or visited[r][c]:
+                 return 0
+             visited[r][c] = True
+             dfs(r+1,c)
+             dfs(r-1,c)
+             dfs(r,c+1)
+             dfs(r,c-1)
+         island_count = 0
+          for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 'L' and not visited[r][c]:
+                    # Found an unvisited land cell, start a DFS to mark the whole island
+                    dfs(r, c)
+                    island_count += 1
+
+        return island_count
