80195075b95d3265d7f46b90958a6602c43c3673
vinayvin576 <130123822+vinayvin576@users.noreply.github.com>
2024-11-11 13:34:07 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,10 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
+    def match_helper(i, j):
+        if j == len(p):
+            return i == len(s) 
+        if j < len(p) and p[j] == '*':
+            return (i < len(s) and match_helper(i + 1, j)) or match_helper(i, j + 1)
+        if i < len(s) and (p[j] == '?' or p[j] == s[i]):
+            return match_helper(i + 1, j + 1)
         return False
+    return match_helper(0, 0)
